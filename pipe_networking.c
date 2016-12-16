7492c93a90f3c1610fcf90d5cc70a517b7f636c8
Haley Zeng <hzeng@stuy.edu>
2016-12-16 11:09:10 -0500

    progress

--- pipe_networking.c
+++ pipe_networking.c
@@ -2,22 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
 
 int server_handshake( int * a) {
   int WKP;
-  if (!(mkfifo("weegee", 0644)) { // Backwards, fix later
-    printf("Oops!\n");
+  if (mkfifo("weegee", 0644)) {
+    printf("Oops! Something went wrong.\n");
     return -1;
   }
+  WKP = open("weegee", O_RDONLY); //blocks here
+  pid_t PP_name;
+  read(WKP, &PP_name, sizeof(pid_t));
+  int PP;
+  PP = open(PP_name, O_WRONLY);
+  write(PP, "connected!", 11);
+  
   return 0;
 }
 
 int client_handshake() {
-  int WKP;
-  if (!(WKP = mkfifo("weegee", 0644))) {
+  int PP;
+  if (mkfifo(getpid(), 0644)) {
     printf("Oops!\n");
     return -1;
   }
+
+  int WKP;
+  WKP = open("weegee", O_WRONLY); //blocks here
+  write(pp, getpid(), sizeof(pid_t));
+  int PP;
+  PP = open(getpid(), O_RDONLY);
+  char whatever[1024];
+  read(PP, whatever, 11);
+  
+  return 0;
 }
 
 
